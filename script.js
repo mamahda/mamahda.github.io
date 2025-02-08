6ea1653eea4d835bf00773dbd77477f056652d7d
mamahda <gbrnmhd89@gmail.com>
2025-02-09 01:08:36 +0700

    update

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const messages = [
   "Are you positive?",
   "Pookie please...",
   "Just think about it!",
-  "If you say no, I will be really sad...",
+  "If you say yes, I will be really sad...",
   "I will be very sad...",
   "I will be very very very sad...",
   "Ok fine, I will stop asking...",
-  "Just kidding, say yes please! ❤️"
+  "Just kidding, say no please! ❤️"
 ];
 
 let messageIndex = 0;
