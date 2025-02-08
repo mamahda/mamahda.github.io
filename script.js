98bc32d0cd934c86dc8cf6d283493d0629a91aae
mamahda <gbrnmhd89@gmail.com>
2025-02-09 00:24:08 +0700

    update

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+  "Are you sure?",
+  "Really sure??",
+  "Are you positive?",
+  "Pookie please...",
+  "Just think about it!",
+  "If you say no, I will be really sad...",
+  "I will be very sad...",
+  "I will be very very very sad...",
+  "Ok fine, I will stop asking...",
+  "Just kidding, say yes please! ❤️"
 ];
 
 let messageIndex = 0;
+let noAlready = false;
+const noButton = document.querySelector('.no-button');
+const yesButton = document.querySelector('.yes-button');
 
 function handleNoClick() {
-    const noButton = document.querySelector('.no-button');
-    const yesButton = document.querySelector('.yes-button');
-    noButton.textContent = messages[messageIndex];
-    messageIndex = (messageIndex + 1) % messages.length;
-    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+  if(noAlready == true) {
+    window.location.href = "yes_page.html";
+  }
+  else {
+    noButton.textContent = "Okay";
+    noAlready = true;
+  }
 }
 
 function handleYesClick() {
-    window.location.href = "yes_page.html";
+  yesButton.textContent = messages[messageIndex];
+  messageIndex = (messageIndex + 1) % messages.length;
+  const currentSize = parseFloat(window.getComputedStyle(noButton).fontSize);
+  noButton.style.fontSize = `${currentSize * 1.5}px`;
 }
